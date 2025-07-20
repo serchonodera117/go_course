fc2a90a62c6c40d4da3371d750e4576071bcf86b
serchonodera117 <sergio.lobo117@gmail.com>
2025-07-19 21:25:21 -0600

    Update: basic operation with functions and console reading already done

--- first_steps/first_steps.go
+++ first_steps/first_steps.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main (){
-	fmt.Println("Holiwi", "ugu")
+	var a int = 10 
+	var b int 
+	var word_print string = "Hola" + " Mundo"
+	
+
+	get_num(&a)
+	
+	fmt.Println(word_print, "\n\n Number: ", a, " will be multioied by the number you write: ")
+	fmt.Scan(&b)
+
+	multiply_num_by(&a, b)
+
+	fmt.Println("Result is: ", a)
+}
+
+func get_num(num *int) {
+	*num *= 3 
+}
+
+func multiply_num_by(num *int, userNum int){
+	*num *= userNum
 }
