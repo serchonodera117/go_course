d9ae76277955232504cebe865ecd0eb69ec62cef
serchonodera117 <sergio.lobo117@gmail.com>
2025-07-21 23:46:27 -0600

    Update: addition of excercises of couritines

--- excercises_part2/problems.go
+++ excercises_part2/problems.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 
 func main(){
 	fmt.Println("--- Excercises ----------")
 
-	execute_excercise_1()
+	// execute_excercise_1()
+	// execute_excercise_2([]string{"hola", "hola", "perro", "oso", "panda", "panda", "hola"})
+	// execute_excercise_3_multiple_goroutines(3)
+	execute_excercise_3_chanel()
 }
 
-//Number one
+//Number one: declare a struct employee, fill up and iterate to receive its values
 func execute_excercise_1(){
 	var employee_slice = []Employee{{ID: 1, Name: "Juan", Position: "Recruiter"}}
 
@@ -25,4 +31,47 @@ type Employee struct {
 	ID int
 	Name string
 	Position string
+}
+
+
+//Number two: map, decclarate a map [string]int that saves times that a word appears into a []String
+
+func execute_excercise_2(arr []string){
+	var mapWords = make(map[string]int)
+	for _, value := range arr {
+		
+		if _,ok:= mapWords[value]; !ok {
+			mapWords[value] = 1
+		}else{
+			mapWords[value] += 1
+		}
+	}
+
+	fmt.Println("my map: ", mapWords)
+}
+
+
+//Number three: launch 5 coroutinas into a groupe
+func execute_excercise_3_multiple_goroutines(n int){
+	var groupToWait sync.WaitGroup
+
+	for i:=0; i<n; i++{
+		groupToWait.Add(1)
+		go func (i int){
+			defer groupToWait.Done()
+			fmt.Println("Goroutine", i)
+		}(i)
+	}
+	groupToWait.Wait()
+	fmt.Println("Goroutines have finished")
+}
+
+func execute_excercise_3_chanel(){
+	ch := make(chan string)
+	go worker(ch)
+	msg := <-ch       //#wait to receive something
+	fmt.Println(msg)
+}
+func worker (ch chan string){
+	ch <- "work ended"
 }
